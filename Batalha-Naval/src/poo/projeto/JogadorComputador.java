69c7d9f1fb9141189600e182164b77d90095e7fe
Dias <>
2018-07-23 19:48:45 -0300

    tentativa de fazer a classe jogadorComputador com o uso do random usando os mesmos metodos da classe jogador

--- Batalha-Naval/src/poo/projeto/JogadorComputador.java
+++ Batalha-Naval/src/poo/projeto/JogadorComputador.java
@@ -3,6 +3,104 @@ package poo.projeto;
 import java.util.Random;
 
 public class JogadorComputador extends Jogador {
+      private Arquivo arq;
+    @Override
+    public char lerOrientacao() {
+        Random jogada = new Random();
+        int orientacao=jogada.nextInt(2);
+        return (char) orientacao;
+    }
+     @Override
+     public int[] lerPosicaoNavioTanque(char orientacao) {
+        int [] posicoesNavioTanque = new String[5];
+        Random marca=new Random();
+        int posicao=marca.nextInt(9);
+        
+      
+        
+        NavioTanque navioTanque = new NavioTanque(orientacao);
+        posicoesNavioTanque = navioTanque.posicoes(posicao);
+        
+        for(int i=0; i<posicoesNavioTanque.length; i++){
+            arq.escrever(arq.getPath(), posicoesNavioTanque[i]);
+        }
+        
+        System.out.println();
+        
+        return  posicoesNavioTanque;
+    }
+     @Override
+    public String[] lerPosicaoPortaAvioes(char orientacao) {
+        String [] posicoesPortaAvioes = new String[4];
+        String posicao;
+        
+        posicao = scan.next();
+        PortaAvioes portAvioes = new PortaAvioes(orientacao);
+        posicoesPortaAvioes = portAvioes.posicoes(posicao);
+        
+        for(int i=0; i<posicoesPortaAvioes.length; i++){
+            arq.escrever(arq.getPath(), posicoesPortaAvioes[i]);
+        }
+        
+        System.out.println();
+        
+        return posicoesPortaAvioes;
+    }
+     @Override
+    public String[] lerPosicaoCruzador(char orientacao) {
+        String [] posicoesCruzador = new String[3];
+        String posicao;
+        
+        posicao = scan.next();
+        Cruzador navioCruzador = new Cruzador(orientacao);
+        posicoesCruzador = navioCruzador.posicoes(posicao);
+        
+        for(int i=0; i<posicoesCruzador.length; i++){
+            arq.escrever(arq.getPath(), posicoesCruzador[i]);
+        }
+        
+        System.out.println();
+        
+        return posicoesCruzador;
+    }
+     @Override
+    public String[] lerPosicaoSubmarino(char orientacao) {
+        String[] posicoesSubmarino = new String[3];
+        String posicao;
+        
+        posicao = scan.next();
+        
+        Submarino sub = new Submarino(orientacao);
+        posicoesSubmarino = sub.posicoes(posicao);
+        
+        for(int i=0; i<posicoesSubmarino.length; i++){
+            arq.escrever(arq.getPath(), posicoesSubmarino[i]);
+        }
+        
+        System.out.println();
+        
+        return posicoesSubmarino;
+    }
+     @Override
+    public String[] lerPosicaoDestruidor(char orientacao) {
+        String[] posicoesDestruidor = new String[2];
+        String posicao;
+        
+        posicao = scan.next();
+        
+        Destruidor dest = new Destruidor(orientacao);
+        posicoesDestruidor = dest.posicoes(posicao);
+        
+        for(int i=0; i<posicoesDestruidor.length; i++){
+            arq.escrever(arq.getPath(), posicoesDestruidor[i]);
+        }
+        
+        System.out.println();
+        
+        return posicoesDestruidor;
+    }
+    
+    
     
     @Override
     public void disporNavios() {
@@ -21,12 +119,74 @@ public class JogadorComputador extends Jogador {
         computador irá realizar diversas jogadas enquanto o jogador ainda
         estiver pensando em realizar algum tiro.
         */
+        boolean controle = true;
+        char orientacao = ' ';
+        String posicao;
+        String[] posicoesPortaAvioes = new String[5];
+        String[] posicoesNavioTanque = new String[4];
+        String[] posicoesCruzador = new String[3];
+        String[] posicoesSubmarino = new String[3];
+        String[] posicoesDestruidor = new String[2];
         
+        System.out.println("-------------------DEFINIÇÃO DA DISPOSIÇÃO DOS "
+                + "NAVIOS!-------------------");
+        /*-------------------------Porta-Aviões-------------------------------*/
+        
+        System.out.print("Digite a orientação cujo deseja inserir "
+                + "o 'Porta-Aviões' [V - vertical] ou [H - horizontal]: ");
+        
+        orientacao = lerOrientacao();
+        
+        System.out.print("Digite a posição cujo deseja inserir "
+                + "o 'Porta-Aviões' : de '[0-9] + [0-9]': ");
+        
+        posicoesPortaAvioes = lerPosicaoPortaAvioes(orientacao);
+        
+        /*-------------------------Navio-Tanque-------------------------------*/
+        
+        System.out.print("Digite a orientação cujo deseja inserir "
+                + "o 'Navio-Tanque' [V - vertical] ou [H - horizontal]: ");
+        orientacao = lerOrientacao();
+        
+        System.out.print("Digite a posição cujo deseja inserir "
+                + "o 'Navio-Tanque' : de '[0-9] + [0-9]': ");
+        
+        posicoesNavioTanque = lerPosicaoNavioTanque(orientacao);
+        
+        /*-----------------------------Cruzador-------------------------------*/
+        
+        System.out.print("Digite a orientação cujo deseja inserir "
+                + "o 'Cruzador' [V - vertical] ou [H - horizontal]: ");
+        orientacao = lerOrientacao();
+        
+        System.out.print("Digite a posição cujo deseja inserir "
+                + "o 'Cruzador' : de '[0-9] + [0-9]': ");
+        posicoesCruzador = lerPosicaoCruzador(orientacao);
+        
+        /*----------------------------Submarino-------------------------------*/
+        
+        System.out.print("Digite a orientação cujo deseja inserir "
+                + "o 'Submarino' [V - vertical] ou [H - horizontal]: ");
+        orientacao = lerOrientacao();
+        
+        System.out.print("Digite a posição cujo deseja inserir "
+                + "o 'Submarino' : de '[0-9] + [0-9]': ");
+        posicoesSubmarino = lerPosicaoSubmarino(orientacao);
+        /*-----------------------------Destruidor-----------------------------*/
+        
+        orientacao = ' ';
+        
+        System.out.print("Digite a orientação cujo deseja inserir "
+                + "o 'Destruidor' [V - vertical] ou [H - horizontal]: ");
+        orientacao = lerOrientacao();
+        
+        System.out.print("Digite a posição cujo deseja inserir "
+                + "o 'Destruidor' : de '[0-9] + [0-9]': ");
+        posicoesDestruidor = lerPosicaoDestruidor(orientacao);
     }
     
     @Override
     public void jogadas(Tabuleiro tab) {
-        
     }
 }
     
