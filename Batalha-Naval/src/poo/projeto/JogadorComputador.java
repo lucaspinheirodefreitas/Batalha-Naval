2577abb203eaf61ff03b47a74b4fc5ac1cdaeba8
Lucas Pinheiro <38235851+lucaspinheirodefreitas@users.noreply.github.com>
2018-07-23 21:15:43 -0300

    Update JogadorComputador.java

--- Batalha-Naval/src/poo/projeto/JogadorComputador.java
+++ Batalha-Naval/src/poo/projeto/JogadorComputador.java
@@ -3,124 +3,13 @@ package poo.projeto;
 import java.util.Random;
 
 public class JogadorComputador extends Jogador {
-      private Arquivo arq;
-    @Override
-    public char lerOrientacao() {
-        Random jogada = new Random();
-        int orientacao=jogada.nextInt(2);
-        return (char) orientacao;
-    }
-     @Override
-     public int[] lerPosicaoNavioTanque(char orientacao) {
-        int [] posicoesNavioTanque = new String[5];
-        Random marca=new Random();
-        int posicao=marca.nextInt(9);
-        
-      
-        
-        NavioTanque navioTanque = new NavioTanque(orientacao);
-        posicoesNavioTanque = navioTanque.posicoes(posicao);
-        
-        for(int i=0; i<posicoesNavioTanque.length; i++){
-            arq.escrever(arq.getPath(), posicoesNavioTanque[i]);
-        }
-        
-        System.out.println();
-        
-        return  posicoesNavioTanque;
-    }
-     @Override
-    public String[] lerPosicaoPortaAvioes(char orientacao) {
-        String [] posicoesPortaAvioes = new String[4];
-        String posicao;
-        
-        posicao = scan.next();
-        PortaAvioes portAvioes = new PortaAvioes(orientacao);
-        posicoesPortaAvioes = portAvioes.posicoes(posicao);
-        
-        for(int i=0; i<posicoesPortaAvioes.length; i++){
-            arq.escrever(arq.getPath(), posicoesPortaAvioes[i]);
-        }
-        
-        System.out.println();
-        
-        return posicoesPortaAvioes;
-    }
-     @Override
-    public String[] lerPosicaoCruzador(char orientacao) {
-        String [] posicoesCruzador = new String[3];
-        String posicao;
-        
-        posicao = scan.next();
-        Cruzador navioCruzador = new Cruzador(orientacao);
-        posicoesCruzador = navioCruzador.posicoes(posicao);
-        
-        for(int i=0; i<posicoesCruzador.length; i++){
-            arq.escrever(arq.getPath(), posicoesCruzador[i]);
-        }
-        
-        System.out.println();
-        
-        return posicoesCruzador;
-    }
-     @Override
-    public String[] lerPosicaoSubmarino(char orientacao) {
-        String[] posicoesSubmarino = new String[3];
-        String posicao;
-        
-        posicao = scan.next();
-        
-        Submarino sub = new Submarino(orientacao);
-        posicoesSubmarino = sub.posicoes(posicao);
-        
-        for(int i=0; i<posicoesSubmarino.length; i++){
-            arq.escrever(arq.getPath(), posicoesSubmarino[i]);
-        }
-        
-        System.out.println();
-        
-        return posicoesSubmarino;
-    }
-     @Override
-    public String[] lerPosicaoDestruidor(char orientacao) {
-        String[] posicoesDestruidor = new String[2];
-        String posicao;
-        
-        posicao = scan.next();
-        
-        Destruidor dest = new Destruidor(orientacao);
-        posicoesDestruidor = dest.posicoes(posicao);
-        
-        for(int i=0; i<posicoesDestruidor.length; i++){
-            arq.escrever(arq.getPath(), posicoesDestruidor[i]);
-        }
-        
-        System.out.println();
-        
-        return posicoesDestruidor;
-    }
     
-    
+    Random gerador = new Random();
     
     @Override
     public void disporNavios() {
-        
-        /*Aqui desenvolver o metodo de disposição dos navios imprimindo frases 
-        após cada inserção de um novo navio, mantendo a ordem de inserção, sendo
-        o primeiro de 5 posições, depois de 4, depois de 3, depois de 3, depois 
-        de 2. Vale lembrar que é interessante usar os metodos existentes na
-        superclasse pois há um DE-PARA de posições incrmenetando 2 no valor
-        digitado pelo usuário. Uma forma como pensei em implementar foi utilizar
-        um laço while que espera o usuário digitar algum caracter ou enter, algo
-        do tipo para que o metodo gere uma nova posicao a ser inserida, fazendo
-        com que assim o arquivo receba uma posição por vez. Fazer algo similar
-        com a implementacao do metodo que irá gerar uma posicao a ser dado
-        tiro, pois se não houver uma trava do tipo esperar um comando, o 
-        computador irá realizar diversas jogadas enquanto o jogador ainda
-        estiver pensando em realizar algum tiro.
-        */
-        boolean controle = true;
-        char orientacao = ' ';
+        char orientacao;
+        boolean controle = false;
         String posicao;
         String[] posicoesPortaAvioes = new String[5];
         String[] posicoesNavioTanque = new String[4];
@@ -132,61 +21,170 @@ public class JogadorComputador extends Jogador {
                 + "NAVIOS!-------------------");
         /*-------------------------Porta-Aviões-------------------------------*/
         
-        System.out.print("Digite a orientação cujo deseja inserir "
-                + "o 'Porta-Aviões' [V - vertical] ou [H - horizontal]: ");
-        
         orientacao = lerOrientacao();
+        posicoesPortaAvioes = lerPosicao(5, orientacao);
         
-        System.out.print("Digite a posição cujo deseja inserir "
-                + "o 'Porta-Aviões' : de '[0-9] + [0-9]': ");
-        
-        posicoesPortaAvioes = lerPosicaoPortaAvioes(orientacao);
+        System.out.println("O computador definiu a posição que será inserido "
+                + "o Porta-Aviões!");
         
         /*-------------------------Navio-Tanque-------------------------------*/
         
-        System.out.print("Digite a orientação cujo deseja inserir "
-                + "o 'Navio-Tanque' [V - vertical] ou [H - horizontal]: ");
         orientacao = lerOrientacao();
+        posicoesNavioTanque = lerPosicao(4, orientacao);
         
-        System.out.print("Digite a posição cujo deseja inserir "
-                + "o 'Navio-Tanque' : de '[0-9] + [0-9]': ");
-        
-        posicoesNavioTanque = lerPosicaoNavioTanque(orientacao);
+        System.out.println("O computador definiu a posição que será inserido "
+                + "o Navio-Tanque!");
         
         /*-----------------------------Cruzador-------------------------------*/
         
-        System.out.print("Digite a orientação cujo deseja inserir "
-                + "o 'Cruzador' [V - vertical] ou [H - horizontal]: ");
         orientacao = lerOrientacao();
+        posicoesCruzador = lerPosicao(3, orientacao);
         
-        System.out.print("Digite a posição cujo deseja inserir "
-                + "o 'Cruzador' : de '[0-9] + [0-9]': ");
-        posicoesCruzador = lerPosicaoCruzador(orientacao);
-        
+        System.out.println("O computador definiu a posição que será inserido "
+                + "o Cruzador!");
         /*----------------------------Submarino-------------------------------*/
         
-        System.out.print("Digite a orientação cujo deseja inserir "
-                + "o 'Submarino' [V - vertical] ou [H - horizontal]: ");
         orientacao = lerOrientacao();
+        posicoesSubmarino = lerPosicao(3, orientacao);
         
-        System.out.print("Digite a posição cujo deseja inserir "
-                + "o 'Submarino' : de '[0-9] + [0-9]': ");
-        posicoesSubmarino = lerPosicaoSubmarino(orientacao);
+        System.out.println("O computador definiu a posição que será inserido "
+                + "o Submarino!");
+        
         /*-----------------------------Destruidor-----------------------------*/
         
-        orientacao = ' ';
-        
-        System.out.print("Digite a orientação cujo deseja inserir "
-                + "o 'Destruidor' [V - vertical] ou [H - horizontal]: ");
         orientacao = lerOrientacao();
+        posicoesDestruidor = lerPosicao(2, orientacao);
         
-        System.out.print("Digite a posição cujo deseja inserir "
-                + "o 'Destruidor' : de '[0-9] + [0-9]': ");
-        posicoesDestruidor = lerPosicaoDestruidor(orientacao);
+        System.out.println("O computador definiu a posição que será inserido "
+                + "o Destruidor!");
+        
+        System.out.println();
+        
+        System.out.println("O computador já definiu a disposição de todos os "
+                + "navios, quando concluir a inserção dos seus Navios, digite "
+                + "'s' para que o computador dê o primeiro tiro!");
+        
+        while(!controle) {
+            char fimDisposicao = scan.next().charAt(0);
+            
+            if(fimDisposicao == 's' || fimDisposicao == 'S') {
+                controle = true;
+            }
+            else {
+                while(fimDisposicao != 's' && fimDisposicao != 'S') {
+                    System.out.println("Letra inválida, digite 's' para "
+                            + "prosseguir com o jogo!");
+                    fimDisposicao = scan.next().charAt(0);
+                }
+                controle = true;
+            }
+        }
+    }
+    
+    @Override
+    public char lerOrientacao() {
+        int orientacao;
+        //gera numeros aleatorios entre 0 e (n-1), neste caso (n = 2)
+        orientacao = gerador.nextInt(2);
+        
+        if(orientacao == 0) {
+            return 'h';
+        }
+        else {
+            return 'v';
+        }
+    }
+    
+    @Override
+    public String[] lerPosicao(int tamanho, char orientacao) {
+        int auxPosicao;
+        String [] posicoes = new String[tamanho];
+        String posicao;
+        
+        /*falta verificar se a posição gerada é válida. 
+        Pois é necessário que a posição seja válida, é interessante implementar
+        um método que sirva para válidar a posição gerada pelo computador e 
+        também sirva para validar a posição digitada pelo usuário
+        */
+        
+        auxPosicao = gerador.nextInt(10);
+        posicao = Integer.toString(auxPosicao);
+        auxPosicao = gerador.nextInt(10);
+        posicao += Integer.toString(auxPosicao);
+        System.out.println(posicao);
+        
+        Navio nav = new Navio(tamanho, orientacao);
+        posicoes = nav.posicoes(posicao);
+        
+        for(int i=0; i<posicoes.length; i++){
+            getArquivo().escrever(getArquivo().getPath(), posicoes[i]);
+        }
+        System.out.println();
+        
+        return posicoes;
     }
     
     @Override
     public void jogadas(Tabuleiro tab) {
+        boolean fim, achou, perdeu;
+        String posicao;
+        perdeu = false;
+        fim = false;
+        achou = false;
+        int alvosAtingidos = 0;
+        
+        System.out.println("--------------------------------AGORA O COMPUTADOR "
+                + "IRÁ DEFINIR AS JOGADAS QUE REALIZARÁ PARA TE ATACAR!!!"
+                + "--------------------------------");
+        
+        System.out.println("Note que será necessário digitar 's' "
+                + "(no console referente as jogadas do computador) "
+                + "após executar "
+                + "sua jogada para que o computador possa realizar sua jogada!");
+        
+        /* 
+        Não fiz nada daqui para baixo
+        */
+        
+        tab.imprimirTabuleiro();
+        
+        System.out.println();
+        
+        while(!fim) {
+            perdeu = getArquivo().verificarFim(getArquivo().getPathAdversario());
+            
+            if(perdeu) {
+                fim = true;
+            }
+            else if(alvosAtingidos == 17) {
+                getArquivo().escrever(getArquivo().getPath(), "fim");
+                fim = true;
+            }
+            else {
+                System.out.print("Digite a posição cujo deseja atingir "
+                +  ": de '[0-9] + [0-9]': ");
+                posicao = scan.next();
+                achou = getArquivo().buscar(getArquivo().getPathAdversario(), posicao);
+                if(achou) {
+                    alvosAtingidos++;
+                }
+                buscarPos(tab, posicao, achou);
+                System.out.println();
+                tab.imprimirTabuleiro();
+                System.out.println();
+            }
+        }
+        System.out.println();
+        if(perdeu) {
+            System.out.println(this.getNome() + ", você foi derrotado!");
+        }
+        else {
+            System.out.println("Fim de jogo, parabéns " + this.getNome() + 
+                    ", você venceu!");
+        }
+        
+        getArquivo().deletarArquivo();
+        
+        System.out.println();
     }
 }
-    
